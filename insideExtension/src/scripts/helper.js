e399f1e01c4ba5a443e57b0236f370601b892d05
xenotime-india <sandeepkhoj@gmail.com>
2018-04-12 14:12:57 +0530

    fixed formating

--- insideExtension/src/scripts/helper.js
+++ insideExtension/src/scripts/helper.js
@@ -1,84 +1,80 @@
 String.prototype.trimEnd = function(c) {
-    if (c)
-        return this.replace(new RegExp(c.escapeRegExp() + "*$"), '');
-    return this.replace(/\s+$/, '');
+  if (c) return this.replace(new RegExp(c.escapeRegExp() + "*$"), "");
+  return this.replace(/\s+$/, "");
 };
 String.prototype.trimStart = function(c) {
-    if (c)
-        return this.replace(new RegExp("^" + c.escapeRegExp() + "*"), '');
-    return this.replace(/^\s+/, '');
+  if (c) return this.replace(new RegExp("^" + c.escapeRegExp() + "*"), "");
+  return this.replace(/^\s+/, "");
 };
 
 String.prototype.escapeRegExp = function() {
-    return this.replace(/[.*+?^${}()|[\]\/\\]/g, "\\$0");
+  return this.replace(/[.*+?^${}()|[\]\/\\]/g, "\\$0");
 };
 
-convertSFDC15To18 = function(sfdcID15){
-    if (sfdcID15.length == 15) {
-        var s = "";
-        for (var i = 0; i < 3; i++) {
-            var f = 0;
-            for (var j = 0; j < 5; j++) {
-                var c = sfdcID15.charAt(i * 5 + j);
-                if (c >= "A" && c <= "Z")
-                    f += 1 << j;
-            }
-            s += "ABCDEFGHIJKLMNOPQRSTUVWXYZ012345".charAt(f);
-        }
-        return sfdcID15 + s;
-    } else {
-        throw "Error : " + sfdcID15 + " has not a length of 15 characters. Current length detected: " + sfdcID15.length + " characters.";
+convertSFDC15To18 = function(sfdcID15) {
+  if (sfdcID15.length == 15) {
+    var s = "";
+    for (var i = 0; i < 3; i++) {
+      var f = 0;
+      for (var j = 0; j < 5; j++) {
+        var c = sfdcID15.charAt(i * 5 + j);
+        if (c >= "A" && c <= "Z") f += 1 << j;
+      }
+      s += "ABCDEFGHIJKLMNOPQRSTUVWXYZ012345".charAt(f);
     }
+    return sfdcID15 + s;
+  } else {
+    throw "Error : " +
+      sfdcID15 +
+      " has not a length of 15 characters. Current length detected: " +
+      sfdcID15.length +
+      " characters.";
+  }
 };
 
 subStrAfterChars = function(str, char, pos) {
-    if(pos=='b')
-        return str.substring(str.indexOf(char) + 1);
-    else if(pos=='a')
-        return str.substring(0, str.indexOf(char));
-    else
-        return str;
+  if (pos == "b") return str.substring(str.indexOf(char) + 1);
+  else if (pos == "a") return str.substring(0, str.indexOf(char));
+  else return str;
 };
 
 getUrlEncodedKey = function(key, query) {
-    if (!query)
-        query = window.location.search;
-    var re = new RegExp("[?|&]" + key + "=(.*?)&");
-    var matches = re.exec(query + "&");
-    if (!matches || matches.length < 2)
-        return "";
-    return decodeURIComponent(matches[1].replace("+", " "));
+  if (!query) query = window.location.search;
+  var re = new RegExp("[?|&]" + key + "=(.*?)&");
+  var matches = re.exec(query + "&");
+  if (!matches || matches.length < 2) return "";
+  return decodeURIComponent(matches[1].replace("+", " "));
 };
 setUrlEncodedKey = function(key, value, query) {
-
-    query = query || window.location.search;
-    var q = query + "&";
-    var re = new RegExp("[?|&]" + key + "=.*?&");
-    if (!re.test(q))
-        q += key + "=" + encodeURIComponent(value);
-    else
-        q = q.replace(re, "&" + key + "=" + encodeURIComponent(value) + "&");
-    q = q.trimStart("&").trimEnd("&");
-    return (q[0]=="?" ? q : q = "?" + q);
+  query = query || window.location.search;
+  var q = query + "&";
+  var re = new RegExp("[?|&]" + key + "=.*?&");
+  if (!re.test(q)) q += key + "=" + encodeURIComponent(value);
+  else q = q.replace(re, "&" + key + "=" + encodeURIComponent(value) + "&");
+  q = q.trimStart("&").trimEnd("&");
+  return q[0] == "?" ? q : (q = "?" + q);
 };
 
 var getServerURL = function() {
   var url = window.location.href;
   var arr = url.split("/");
   return arr[0] + "//" + arr[2];
-}
+};
 
-var __getCookie = function(c_name){
-  var i,x,y,ARRcookies=document.cookie.split(";");
-  for (i=0;i<ARRcookies.length;i++){
-    x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
-    y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
-    x=x.replace(/^\s+|\s+$/g,"");
-    if (x==c_name){
+var __getCookie = function(c_name) {
+  var i,
+    x,
+    y,
+    ARRcookies = document.cookie.split(";");
+  for (i = 0; i < ARRcookies.length; i++) {
+    x = ARRcookies[i].substr(0, ARRcookies[i].indexOf("="));
+    y = ARRcookies[i].substr(ARRcookies[i].indexOf("=") + 1);
+    x = x.replace(/^\s+|\s+$/g, "");
+    if (x == c_name) {
       return unescape(y);
     }
   }
-}
+};
 
 var getObjectIdFromUri = function(e) {
   if (isBlank(e)) return null;
@@ -87,58 +83,96 @@ var getObjectIdFromUri = function(e) {
   var n = t.pathname.match(/\/[a-zA-Z0-9]{15,18}/);
   if (isBlank(n) || 0 == n.length) return null;
   var r = n[0].replace(new RegExp("/", "g"), "");
-  return r
+  return r;
 };
 
 var getParser = function(e) {
   var t = document.createElement("a");
-  return t.href = e, t
-}
+  return (t.href = e), t;
+};
 
 var isBlank = function(e) {
-  return "undefined" == typeof e || null === e ? !0 : "string" == typeof e && ("undefined" === e || "null" === e || "" === e)
-}
+  return "undefined" == typeof e || null === e
+    ? !0
+    : "string" == typeof e && ("undefined" === e || "null" === e || "" === e);
+};
 
 var escapeString = function(e) {
-  return String(e).replace(/\r?\n/g, "\\n").replace(/\r?\t/g, "\\t").replace(/'/g, "'").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/&/g, "&amp;").replace(/"/g, "&quot;").replace(/'/g, "&#39;")
+  return String(e)
+    .replace(/\r?\n/g, "\\n")
+    .replace(/\r?\t/g, "\\t")
+    .replace(/'/g, "'")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/&/g, "&amp;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#39;");
 };
 
 var getColLable = function(e) {
-  var t = jQuery(e).clone().children().remove().end().text();
-  return isBlank(t) && (t = jQuery(e).children(":visible").first().clone().children().remove().end().text()), t
+  var t = jQuery(e)
+    .clone()
+    .children()
+    .remove()
+    .end()
+    .text();
+  return (
+    isBlank(t) &&
+      (t = jQuery(e)
+        .children(":visible")
+        .first()
+        .clone()
+        .children()
+        .remove()
+        .end()
+        .text()),
+    t
+  );
 };
 
 var getObjectValues = function() {
   var e = [];
-  return jQuery("#bodyCell").find(".detailList").each(function() {
-    jQuery(this).find(".labelCol").each(function() {
-      var t = getColLable(this);
-      if (!isBlank(t) && !isBlank(t.replace(/((\s*\S+)*)\s*/, "$1"))) {
-        var n = jQuery(this).next("td").first(),
-          r = n.text();
-        r = escapeString(r);
-        var i = {
-          label: t,
-          value: r
-        };
-        if (n.find("a").length > 0) {
-          var o = n.find("a").first(),
-            s = o.attr("href"),
-            a = o.attr("id");
-          if (!isBlank(s) && !isBlank(a) && a.indexOf("lookup") >= 0) {
-            var c = o.get(0).pathname.replace(/\//g, "");
-            i.reference = c
-          }
-        }
-        if (n.find(".checkImg").length > 0) {
-          var l = n.find(".checkImg").first().attr("src");
-          i.value = !(l.indexOf("unchecked") >= 0)
-        }
-        e.push(i)
-      }
-    })
-  }), e
-}
+  return (
+    jQuery("#bodyCell")
+      .find(".detailList")
+      .each(function() {
+        jQuery(this)
+          .find(".labelCol")
+          .each(function() {
+            var t = getColLable(this);
+            if (!isBlank(t) && !isBlank(t.replace(/((\s*\S+)*)\s*/, "$1"))) {
+              var n = jQuery(this)
+                  .next("td")
+                  .first(),
+                r = n.text();
+              r = escapeString(r);
+              var i = {
+                label: t,
+                value: r
+              };
+              if (n.find("a").length > 0) {
+                var o = n.find("a").first(),
+                  s = o.attr("href"),
+                  a = o.attr("id");
+                if (!isBlank(s) && !isBlank(a) && a.indexOf("lookup") >= 0) {
+                  var c = o.get(0).pathname.replace(/\//g, "");
+                  i.reference = c;
+                }
+              }
+              if (n.find(".checkImg").length > 0) {
+                var l = n
+                  .find(".checkImg")
+                  .first()
+                  .attr("src");
+                i.value = !(l.indexOf("unchecked") >= 0);
+              }
+              e.push(i);
+            }
+          });
+      }),
+    e
+  );
+};
 
 var getFieldCode = function(e, t) {
   if (t.label !== e.label) return null;
@@ -148,65 +182,94 @@ var getFieldCode = function(e, t) {
   if (isBlank(r)) return null;
   if ("Formula" == r || "Auto Number" == r) return null;
   var i = e.value;
-  if (isBlank(i) && (i = " "), i = i.toString().rtrim(), isBlank(i)) return "null";
+  if ((isBlank(i) && (i = " "), (i = i.toString().rtrim()), isBlank(i)))
+    return "null";
   if ("RecordType" == r) {
-    if (i = i.replace(/\[(.*)\]/g, "").rtrim(), 0 == t.recordTypes.length) return null;
+    if (((i = i.replace(/\[(.*)\]/g, "").rtrim()), 0 == t.recordTypes.length))
+      return null;
     for (var o = 0; o < t.recordTypes.length; o++) {
       var a = t.recordTypes[o];
-      if (i == a.label) return "'" + a.id + "'"
+      if (i == a.label) return "'" + a.id + "'";
     }
-    return null
+    return null;
   }
-  return "Currency" == r || "Number" == r || "Percent" == r ? (i = i.replace(/\,/g, "").replace(/\%/g, ""), getNumber(i)) : "Checkbox" == r ? 1 == i || "true" == i || 1 == i || "1" == i ? "true" : "false" : "Date" == r ? "Date.valueOf('" + i.replace(/\//g, "-") + "')" : "Date/Time" == r ? "Datetime.valueOf('" + i.replace(/\//g, "-") + ":00')" : "Master-Detail" == r || "Lookup" == r ? "'" + e.reference + "'" : "'" + punescape(i) + "'"
-}
+  return "Currency" == r || "Number" == r || "Percent" == r
+    ? ((i = i.replace(/\,/g, "").replace(/\%/g, "")), getNumber(i))
+    : "Checkbox" == r
+      ? 1 == i || "true" == i || 1 == i || "1" == i
+        ? "true"
+        : "false"
+      : "Date" == r
+        ? "Date.valueOf('" + i.replace(/\//g, "-") + "')"
+        : "Date/Time" == r
+          ? "Datetime.valueOf('" + i.replace(/\//g, "-") + ":00')"
+          : "Master-Detail" == r || "Lookup" == r
+            ? "'" + e.reference + "'"
+            : "'" + punescape(i) + "'";
+};
 
 var punescape = function(e) {
-  return String(e).replace(/&lt;/g, "<").replace(/&gt;/g, ">").replace(/&amp;/g, "&").replace(/&quot;/g, '"').replace(/&#39;/g, "'")
-}
+  return String(e)
+    .replace(/&lt;/g, "<")
+    .replace(/&gt;/g, ">")
+    .replace(/&amp;/g, "&")
+    .replace(/&quot;/g, '"')
+    .replace(/&#39;/g, "'");
+};
 
 var getNumber = function(e) {
   if (isBlank(e)) return null;
   var t = e.match(/([1-9]\d*|0)(\.\d+)?/g);
-  return isBlank(t) ? null : t[0]
-}
+  return isBlank(t) ? null : t[0];
+};
 
 var getSpaceByNum = function(e) {
   for (var t = "", n = 0; e > n; n++) t += " ";
-  return t
-}
+  return t;
+};
 
 function showAlert(mess) {
-    if (sfdcPage.dialogs['SFDCDialog'] == null) { // checking if SFDCDialog modal popup already created on page.
+  if (sfdcPage.dialogs["SFDCDialog"] == null) {
+    // checking if SFDCDialog modal popup already created on page.
 
-        sfdcPage.dialogs['SFDCDialog'] = new SimpleDialog('SFDCDialog', false); // creating modal popup with name ‘SFDCDialog’
+    sfdcPage.dialogs["SFDCDialog"] = new SimpleDialog("SFDCDialog", false); // creating modal popup with name ‘SFDCDialog’
 
-        sfdcPage.dialogs['SFDCDialog'].title = "Salesforce Deployment Helper - Xenotime"; // setting title of popup
+    sfdcPage.dialogs["SFDCDialog"].title =
+      "Salesforce Deployment Helper - Xenotime"; // setting title of popup
 
-        sfdcPage.dialogs['SFDCDialog'].isMovable = false; //set true if want movable
+    sfdcPage.dialogs["SFDCDialog"].isMovable = false; //set true if want movable
 
-        sfdcPage.dialogs['SFDCDialog'].displayX = true; // set true if want close button on header
+    sfdcPage.dialogs["SFDCDialog"].displayX = true; // set true if want close button on header
 
-        sfdcPage.dialogs['SFDCDialog'].extraClass = "" // use to set any extra style class if wanted
+    sfdcPage.dialogs["SFDCDialog"].extraClass = ""; // use to set any extra style class if wanted
 
-        sfdcPage.dialogs['SFDCDialog'].width = 346; // set size of popup default = 400
+    sfdcPage.dialogs["SFDCDialog"].width = 346; // set size of popup default = 400
 
-        sfdcPage.dialogs['SFDCDialog'].isModal = true; // set true if want block background.
+    sfdcPage.dialogs["SFDCDialog"].isModal = true; // set true if want block background.
 
-        sfdcPage.dialogs['SFDCDialog'].createDialog(); // finally call this method to create modal pop up  and append to current page.
+    sfdcPage.dialogs["SFDCDialog"].createDialog(); // finally call this method to create modal pop up  and append to current page.
+  }
+  var message =
+    '<table border="0"><tbody><tr><td style="vertical-align: top"><img src="/s.gif" class="confirmLarge" alt="Confirm"></td><td style="padding-left: 8px; vertical-align: top; line-height: 16px"><p>' +
+    mess +
+    "</p></td></tr></tbody></table>";
+  sfdcPage.dialogs["SFDCDialog"].setContentInnerHTML(
+    "<div>" + message + "</div>"
+  );
 
-    }
-    var message = '<table border="0"><tbody><tr><td style="vertical-align: top"><img src="/s.gif" class="confirmLarge" alt="Confirm"></td><td style="padding-left: 8px; vertical-align: top; line-height: 16px"><p>'+mess+'</p></td></tr></tbody></table>'
-    sfdcPage.dialogs['SFDCDialog'].setContentInnerHTML('<div>' + message + '</div>');
-
-    sfdcPage.dialogs['SFDCDialog'].show();// show modal popup
+  sfdcPage.dialogs["SFDCDialog"].show(); // show modal popup
 }
 
 String.prototype.rtrim = function() {
-  return this.replace(/((\s*\S+)*)\s*/, "$1")
+  return this.replace(/((\s*\S+)*)\s*/, "$1");
 };
 String.prototype.ltrim = function() {
-  return this.replace(/\s*((\S+\s*)*)/, "$1")
+  return this.replace(/\s*((\S+\s*)*)/, "$1");
 };
 String.prototype.clearSpace = function() {
-  return isBlank(this) ? this : this.replace(/\n/g, "").replace(/\t/g, "").replace(/ /g, "")
+  return isBlank(this)
+    ? this
+    : this.replace(/\n/g, "")
+        .replace(/\t/g, "")
+        .replace(/ /g, "");
 };
--- insideExtension/src/scripts/popup.js
+++ insideExtension/src/scripts/popup.js
@@ -1,72 +1,100 @@
-$(function () {
-
-  chrome.tabs.query({
-    active: !0,
-    currentWindow: !0
-  }, function(e) {
-    if (!isBlank(e[0])) {
-      var t = getParser(e[0].url);
-      var re = /.com\/(\w{15})/g;
-      var match = re.exec(e[0].url);
-      if (t.hostname.indexOf("salesforce.com") >= 0 && match) {
-        return callPage({mode : "GET_METADATA"});
+$(function() {
+  chrome.tabs.query(
+    {
+      active: !0,
+      currentWindow: !0
+    },
+    function(e) {
+      if (!isBlank(e[0])) {
+        var t = getParser(e[0].url);
+        var re = /.com\/(\w{15})/g;
+        var match = re.exec(e[0].url);
+        if (t.hostname.indexOf("salesforce.com") >= 0 && match) {
+          return callPage({ mode: "GET_METADATA" });
+        } else {
+          jQuery("#some-code").html(
+            "\n\nIt looks like you are opening me on a \nnon-Salesforce.com domain or non-sobject detail page...\n\n\n:-("
+          );
+          jQuery(".holder").hide();
+        }
       }
-      else {
-        jQuery('#some-code').html('\n\nIt looks like you are opening me on a \nnon-Salesforce.com domain or non-sobject detail page...\n\n\n:-(');
-        jQuery('.holder').hide();
+    }
+  );
+
+  chrome.runtime.onMessage.addListener(function(e, t, n) {
+    if (e.status == 200) {
+      if (e.action == "GET_METADATA") {
+        makeApexCode({
+          fields: e.fields,
+          values: e.values,
+          sobject: e.sobject
+        });
       }
     }
   });
-
-  chrome.runtime.onMessage.addListener(function(e, t, n) {
-    if(e.status == 200) {
-      if (e.action == 'GET_METADATA') {
-        makeApexCode({fields: e.fields, values: e.values, sobject: e.sobject });
-      }
-    }
-  })
 });
 
 var callPage = function(data) {
-  chrome.tabs.query({
-    active: !0,
-    currentWindow: !0
-  }, function(tab) {
-    if (0 != tab.length) {
-      try {
-        chrome.tabs.executeScript(tab[0].id, {
-          code: "var data = " + JSON.stringify(data) + ";"
-        }, function () {
-          chrome.tabs.executeScript(tab[0].id, {
-            file: 'scripts/inlineExecute.min.js'
-          }, function() {
-          })
-        })
-      } catch (ex) {
-        return {error: ex};
+  chrome.tabs.query(
+    {
+      active: !0,
+      currentWindow: !0
+    },
+    function(tab) {
+      if (0 != tab.length) {
+        try {
+          chrome.tabs.executeScript(
+            tab[0].id,
+            {
+              code: "var data = " + JSON.stringify(data) + ";"
+            },
+            function() {
+              chrome.tabs.executeScript(
+                tab[0].id,
+                {
+                  file: "scripts/inlineExecute.min.js"
+                },
+                function() {}
+              );
+            }
+          );
+        } catch (ex) {
+          return { error: ex };
+        }
       }
     }
-  })
-}
+  );
+};
 
 var makeApexCode = function(e) {
   var fields = {};
   e.fields.forEach(function(element) {
-    var data  = {};
-    data[element.name] = { label: element.label, type:element.type  };
-    fields : Object.assign(fields,data);
+    var data = {};
+    data[element.name] = { label: element.label, type: element.type };
+    fields: Object.assign(fields, data);
   });
   var values = e.values;
-  if (isBlank(fields)) return showCustomDomain({
-    error: !0
-  });
+  if (isBlank(fields))
+    return showCustomDomain({
+      error: !0
+    });
   var r = getObjectIdFromUri(window.location.href),
     i = e.sobject.name;
   if (isBlank(i)) return alert("sObject ERROR!");
-  for (var o = i + " sobj = new " + i + "(", a = o, s = [], l = [], c = 0, u = 0, d = 0; d < values.length; d++) {
+  for (
+    var o = i + " sobj = new " + i + "(",
+      a = o,
+      s = [],
+      l = [],
+      c = 0,
+      u = 0,
+      d = 0;
+    d < values.length;
+    d++
+  ) {
     var f = values[d],
       p = f.value;
-    isBlank(p) && (p = " "), p = p.toString().rtrim();
+    isBlank(p) && (p = " "), (p = p.toString().rtrim());
     for (var h in fields)
       if ("CreatedBy" != h && "LastModifiedBy" != h && "Owner" != h) {
         var g = fields[h];
@@ -80,7 +108,7 @@ var makeApexCode = function(e) {
             isBlank: isBlank(p)
           });
           l.push("// " + f.label);
-          c = v.length > c ? v.length : c
+          c = v.length > c ? v.length : c;
         }
       }
   }
@@ -92,13 +120,13 @@ var makeApexCode = function(e) {
     if (!b.isBlank) {
       y++;
       var T = y == u ? "" : ",";
-      o += v + T + w + x + l[d]
+      o += v + T + w + x + l[d];
     }
     var T = d == s.length - 1 ? "" : ",";
-    a += v + T + w + x + l[d]
+    a += v + T + w + x + l[d];
   }
-  o += "\n);\ninsert sobj;", a += "\n);\ninsert sobj;";
-  jQuery('#some-code').html(o);
-  Prism.highlightElement(jQuery('#some-code')[0]);
-  jQuery('.holder').hide();
-}
+  (o += "\n);\ninsert sobj;"), (a += "\n);\ninsert sobj;");
+  jQuery("#some-code").html(o);
+  Prism.highlightElement(jQuery("#some-code")[0]);
+  jQuery(".holder").hide();
+};
